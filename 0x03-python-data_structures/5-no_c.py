a43257ae770aba8972898df5166a8eea3cd03d04
lepc1972 <githublepc@gmail.com>
2021-05-05 19:09:16 -0500

    no-c

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,5 +2,5 @@ def no_c(my_string):
     noc_string = ""
     for i in my_string:
         if (i != "c" and i != "C"):
-            noc_string =noc_string + i
+            noc_string = noc_string + i
     return noc_string
