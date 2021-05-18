43096f3dbbdd43e0ea8db62ef6d7eb9320862a3b
lepc1972 <githublepc@gmail.com>
2021-05-18 21:57:02 +0000

    adding comment

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
-class Square:
-    __size = 0
+"""Module to create __init__ method and .__size"""
 
-    def __init__(self, new_size=0):
-        """Initialize class."""
-        if new_size is not 0:
-            self.__size = new_size
+
+class Square:
+    """Square with private instance attribute"""
+    def __init__(self, size):
+        """Initialize data"""
+        self.__size = size
+        """Private instantiation of size"""
