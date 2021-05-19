4efd066143bffd34b3d889e56107989e17c96feb
lepc1972 <githublepc@gmail.com>
2021-05-19 01:00:00 +0000

    advanced circle

---
+++ 0x06-python-classes/103-magic_class.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""Python source code that produces exact byte code"""
+
+
+import math
+
+
+class MagicClass:
+    """class to create bytecode in python"""
+    def __init__(self, radius=0):
+        """create a Circle object
+        Args:
+            radius (int or float): the radius of this circle
+        """
+        self.__radius = 0
+        if type(radius) is not int and type(radius) is not float:
+            raise TypeError('radius must be a number')
+        self.__radius = radius
+
+    def area(self):
+        """get this circle's area
+        Returns:
+            float: this circle's area
+        """
+        return self.__radius ** 2 * math.pi
+
+    def circumference(self):
+        """get this circle's circumference
+        Returns:
+            float: this circle's circumference
+        """
+        return 2 * math.pi * self.__radius
