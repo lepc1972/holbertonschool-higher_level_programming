fb714d8630b3077768e14faf2a296eeb1f3997c4
lepc1972 <githublepc@gmail.com>
2021-05-20 12:14:22 -0500

    fix trailing

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -8,7 +8,7 @@ class Rectangle:
     def __init__(self, width=0, height=0):
         """Initialize data"""
         self.width = width
-        self.height = height 
+        self.height = height
 
     @property
     def width(self):
