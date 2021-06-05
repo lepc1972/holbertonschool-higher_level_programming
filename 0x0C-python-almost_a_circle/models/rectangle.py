543b41bd44ddd57673135ec5ecda7f092a4e2b35
lepc1972 <githublepc@gmail.com>
2021-06-05 11:31:34 -0500

    class Rectangle - 2

---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""This module creates class Rectangle"""
+
+
+from models.base import Base
+
+
+class Rectangle(Base):
+    """Creation of class Rectangle that inherits from Base"""
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """init Rectangle class"""
+        super().__init__(id)
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+    
+
+
+    
+
+
+    
+
+    
+
