9f7daa3cfd92ae06c421c08676a5e89167f0c0b4
lepc1972 <githublepc@gmail.com>
2021-06-06 15:37:33 -0500

    return stringof list_dict 14-main

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -17,3 +17,11 @@ class Base():
             self.id = self.__nb_objects
         else:
             self.id = id
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """returns string representation of list_dictionaries"""
+        if list_dictionaries is None or len(list_dictionaries) <= 0:
+            return ("[]")
+        else:
+            return (json.dumps(list_dictionaries))
