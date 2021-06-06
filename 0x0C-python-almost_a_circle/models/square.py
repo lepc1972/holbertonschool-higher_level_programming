8134294b1c087312784f0db76e280a5d4e871f95
lepc1972 <githublepc@gmail.com>
2021-06-06 09:25:15 -0500

    square fixes n 10-main.py

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -10,3 +10,10 @@ class Square(Rectangle):
     def __init__(self, size, x=0, y=0, id=None):
         """init square class"""
         super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        '''
+        Setting str representation
+        '''
+        return '[Square] ({}) {}/{} - {}'\
+            .format(self.id, self.x, self.y, self.width)
