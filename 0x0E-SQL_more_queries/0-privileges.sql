1ee42f9ee510c83ff9890e0b99231ae9726907c0
lepc1972 <githublepc@gmail.com>
2021-07-13 20:16:00 +0000

    fix 0

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,5 +1,7 @@
--- list granted privileges for 2 users
--- first user
+-- Script to list all privileges of users `user_0d_1` and `user_0d_2`
+
+-- List privileges for user1
 SHOW GRANTS FOR user_0d_1@localhost;
--- second user
+
+-- List privileges for user2
 SHOW GRANTS FOR user_0d_2@localhost;
