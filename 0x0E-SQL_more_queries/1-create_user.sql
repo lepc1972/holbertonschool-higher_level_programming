390ce39ac69eb948085b9aca452420bac097936c
lepc1972 <githublepc@gmail.com>
2021-07-13 20:24:46 +0000

    delete

--- 0x0E-SQL_more_queries/1-create_user.sql
+++
@@ -1,6 +0,0 @@
--- create a new user with full privileges on this server
--- create user
-CREATE USER IF NOT EXISTS user_0d_1@localhost
-IDENTIFIED BY 'user_0d_1_pwd';
--- grant privileges
-GRANT ALL ON *.* TO user_0d_1@localhost;
