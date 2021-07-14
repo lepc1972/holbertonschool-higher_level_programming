74c060f0a8ce3f7867d0d265f7d46c9e1292d1bf
lepc1972 <githublepc@gmail.com>
2021-07-14 03:21:27 +0000

    genre

---
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -0,0 +1,7 @@
+-- list only shows without genre
+SELECT `tv_shows`.`title`, `tv_show_genres`.`genre_id`
+FROM `tv_show_genres` RIGHT OUTER JOIN `tv_shows`
+ON `tv_shows`.`id` = `tv_show_genres`.`show_id`
+WHERE `tv_show_genres`.`genre_id` IS NULL
+ORDER BY `tv_shows`.`title`;
+
