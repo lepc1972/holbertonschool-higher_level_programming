66b1c616d84ed56079246fd0f20081ec01711e01
lepc1972 <githublepc@gmail.com>
2021-07-14 03:55:13 +0000

    commedy

--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -1,6 +1,6 @@
--- listing the genres associated with Dexter
-SELECT `tv_genres`.`name` FROM `tv_shows`
+-- listing only comedy shows
+SELECT `tv_shows`.`title` FROM `tv_shows`
 INNER JOIN `tv_show_genres` ON `tv_shows`.`id` = `tv_show_genres`.`show_id`
 INNER JOIN `tv_genres` ON `tv_show_genres`.`genre_id` = `tv_genres`.`id`
-WHERE `tv_shows`.`title` = "Dexter"
-ORDER BY `tv_genres`.`name` ASC;
+WHERE `tv_genres`.`name` = "Comedy"
+ORDER BY `tv_shows`.`title` ASC;
