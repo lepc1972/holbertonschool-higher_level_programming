30044883f433ff1dbe6acc33899ae225c69750b6
lepc1972 <githublepc@gmail.com>
2021-08-03 13:07:11 -0500

    semistandard fixes

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,22 +1,22 @@
 #!/usr/bin/node
 
 class Rectangle {
-    constructor (w, h) {
-      if (w > 0 && h > 0) {
-        this.width = w;
-        this.height = h;
-      }
-    }
-  
-    print () {
-      for (let x = 0; x < this.height; x++) {
-        let row = '';
-        for (let y = 0; y < this.width; y++) {
-          row = row + 'X';
-        }
-        console.log(row);
-      }
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
     }
   }
-  
-  module.exports = Rectangle;
+
+  print () {
+    for (let x = 0; x < this.height; x++) {
+      let row = '';
+      for (let y = 0; y < this.width; y++) {
+        row = row + 'X';
+      }
+      console.log(row);
+    }
+  }
+}
+
+module.exports = Rectangle;
