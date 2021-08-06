820f3c103b36af24b0004d38f36a03d3827766fe
lepc1972 <githublepc@gmail.com>
2021-08-06 19:30:30 +0000

    states with letter a

---
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""script tha list all states that have letter a"""
+
+from sys import argv
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == '__main__':
+    eng = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(argv[1], argv[2], argv[3]),
+                           pool_pre_ping=True)
+
+    Base.metadata.create_all(eng)
+
+    session = sessionmaker(bind=eng)()
+
+    state_a = session.query(State).filter(State.name.like('%a%')).\
+        order_by(State.id).all()
+
+    for state in state_a:
+        print("{}: {}".format(state.id, state.name))
+    session.close()
