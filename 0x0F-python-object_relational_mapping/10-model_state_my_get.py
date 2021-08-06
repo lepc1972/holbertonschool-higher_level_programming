5139a6ccd25a224f741e1c5f95514fb03933b8d6
lepc1972 <githublepc@gmail.com>
2021-08-06 21:06:18 +0000

    10-texas

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+Script that prints the first
+State object from the database hbtn_0e_6_usa
+"""
+
+from sys import argv
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}".
+                           format(argv[1], argv[2],
+                                  argv[3]), pool_pre_ping=True)
+
+    Session = sessionmaker(bind=engine)
+    ses = Session()
+
+    Base.metadata.create_all(engine)
+
+    state = ses.query(State).\
+        filter(State.name == argv[4]).order_by(State.id).all()
+    if state:
+        print("{}".format(state[0].id))
+    else:
+        print("Not found")
+    ses.close()
