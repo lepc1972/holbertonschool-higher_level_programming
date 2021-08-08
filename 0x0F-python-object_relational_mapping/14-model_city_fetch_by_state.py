ee38dd15b1e5410b929cad866bcd5e0346c1dd17
lepc1972 <githublepc@gmail.com>
2021-08-08 21:31:21 +0000

    14

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-"""list all cities of database"""
-
+"""
+script that prints all City objects from the database
+"""
 from sys import argv
 from model_state import Base, State
 from model_city import City
@@ -13,10 +14,12 @@ if __name__ == "__main__":
                            format(argv[1], argv[2],
                                   argv[3]), pool_pre_ping=True)
 
+    Base.metadata.create_all(engine)
+
     Session = sessionmaker()
     Session.configure(bind=engine)
     session = Session()
-    query = session.query(State, City).filter(State.id == City.state_id).order_by(City.id)
+    query = session.query(State, City).filter(State.id == City.state_id).all()
     for result in query:
         print("{}: ({}) {}".
               format(result.State.name, result.City.id, result.City.name))
--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -1,17 +1,20 @@
 #!/usr/bin/python3
+"""
+City class - MySQLAlchemy module
+"""
 from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import relationship
 from model_state import Base, State
+from sqlalchemy.orm import relationship
 
 
 class City(Base):
-    __tablename__ = "cities"
+    """
+    City class
+    """
 
-    id = Column(Integer,
-                unique=True,
-                autoincrement=True,
-                nullable=False,
-                primary_key=True)
+    __tablename__ = 'cities'
+    id = Column(Integer, nullable=False, autoincrement=True, primary_key=True)
     name = Column(String(128), nullable=False)
-    state_id = Column(Integer, ForeignKey("State.id"))
+    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
+    state = relationship("State")
