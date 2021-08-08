edfe5e58ac036db1b71693c8ab66ce6d3c38ca95
lepc1972 <githublepc@gmail.com>
2021-08-08 13:10:37 +0000

    13 delete

---
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+'''Script to change State with id 2 to New Mexcio'''
+
+from sys import argv
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == '__main__':
+    eng = create_engine(
+        'mysql+mysqldb://{}:{}@localhost/{}'
+        .format(argv[1], argv[2], argv[3]), pool_pre_ping=True)
+
+    Base.metadata.create_all(eng)
+
+    ses = sessionmaker(bind=eng)()
+
+    ses.query(State).filter(State.name.like('%a%')).\
+        delete(synchronize_session=False)
+
+    ses.commit()
+    ses.close()
